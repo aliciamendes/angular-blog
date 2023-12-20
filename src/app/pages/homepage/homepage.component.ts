ffdc7063f65e6b7e5bdaa63dd7fdba7fd70fac60
Alícia B M <aliealie.am@gmail.com>
2023-12-20 11:11:38 -0300

    finished components

--- src/app/pages/homepage/homepage.component.ts
+++ src/app/pages/homepage/homepage.component.ts
@@ -2,12 +2,18 @@ import { Component } from '@angular/core';
 import { MenuTittleComponent } from '../../components/menu-tittle/menu-tittle.component';
 import { BigCardComponent } from '../../components/big-card/big-card.component';
 import { SmallCardComponent } from '../../components/small-card/small-card.component';
+import { MenuBarComponent } from '../../components/menu-bar/menu-bar.component';
 
 @Component({
   selector: 'app-homepage',
   standalone: true,
   templateUrl: './homepage.component.html',
   styleUrl: './homepage.component.css',
-  imports: [MenuTittleComponent, BigCardComponent, SmallCardComponent],
+  imports: [
+    MenuTittleComponent,
+    BigCardComponent,
+    SmallCardComponent,
+    MenuBarComponent,
+  ],
 })
 export class HomepageComponent {}
